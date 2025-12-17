c078a2204e73b297017d2ad16f97315e8cdf34e4
Gilbert <27494820+Gilbert-S@users.noreply.github.com>
2025-12-17 14:10:34 +0100

    dependency & security updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from "@sveltejs/adapter-static"
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+import {vitePreprocess} from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,7 +12,7 @@ const config = {
       assets: "build"
     }),
     prerender: {
-      entries: []
+      entries: ['/']
     },
     paths: {
       base: process.env.BASE_PATH ? '/' + process.env.BASE_PATH : ''
